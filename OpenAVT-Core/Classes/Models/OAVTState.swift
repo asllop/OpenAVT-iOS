e607a91c7e1315ddf21edb18f062b9da1db7a2a7
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-10-23 18:23:16 +0200

    Add autodocs for OAVTState.

--- OpenAVT-Core/Classes/Models/OAVTState.swift
+++ OpenAVT-Core/Classes/Models/OAVTState.swift
@@ -8,22 +8,39 @@
 
 import Foundation
 
+/// An OpenAVT state.
 open class OAVTState {
+    /// Media did request flag.
     public var didMediaRequest = false
+    /// Player set flag.
     public var didPlayerSet = false
+    /// Stream did load flag.
     public var didStreamLoad = false
+    /// Stream did start flag.
     public var didStart = false
+    /// Player in buffer state flag.
     public var isBuffering = false
+    /// Player in pause state flag.
     public var isPaused = false
+    /// Player in seek state flag.
     public var isSeeking = false
+    /// Playback finished flag.
     public var didFinish = false
+    /// Player in Ad break flag.
     public var inAdBreak = false
+    /// Player playing an Ad flag.
     public var inAd = false
     
     //TODO: flags for all possible states/events/blocks
     
+    /**
+     Init a new OAVTState.
+     
+     - Returns: A new OAVTState instance.
+    */
     public init() {}
     
+    /// Reset the state.
     public func reset() {
         didMediaRequest = false
         didPlayerSet = false
