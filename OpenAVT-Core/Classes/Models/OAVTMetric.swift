697fac5be274ae49558d1e78a32459bcc1db4981
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-12-19 19:50:49 +0100

    Implement 2 types of value.

--- OpenAVT-Core/Classes/Models/OAVTMetric.swift
+++ OpenAVT-Core/Classes/Models/OAVTMetric.swift
@@ -11,24 +11,67 @@ import Foundation
 open class OAVTMetric {
     
     private let metricName: String
-    private let metricValue: Double
+    private let metricValueD: Double?
+    private let metricValueI: Int?
     
     /**
      Init a new OAVTMetric, providing name and value.
      
      - Parameters:
         - name: Metric name.
-        - value: Metric value.
+        - value: Metric value, double.
      
      - Returns: A new OAVTMetric instance.
     */
     public init(name: String, value: Double) {
         self.metricName = name
-        self.metricValue = value
+        self.metricValueD = value
+        self.metricValueI = nil
+    }
+    
+    /**
+     Init a new OAVTMetric, providing name and value.
+     
+     - Parameters:
+        - name: Metric name.
+        - value: Metric value, integer.
+     
+     - Returns: A new OAVTMetric instance.
+    */
+    public init(name: String, value: Int) {
+        self.metricName = name
+        self.metricValueI = value
+        self.metricValueD = nil
+    }
+
+    /**
+     Get metric name.
+     
+     - Returns: Name.
+    */
+    public func getName() -> String {
+        return self.metricName
+    }
+    
+    /**
+     Get metric value.
+     
+     - Returns: Value.
+    */
+    public func getValue() -> Any {
+        if let i = self.metricValueD {
+            return i
+        }
+        else if let i = self.metricValueI {
+            return i
+        }
+        else {
+            return Double.nan
+        }
     }
     
     /// Generate a readable description.
     public var description : String {
-        return "<OAVTMetric : Name = \(metricName) , Value = \(metricValue)>"
+        return "<OAVTMetric : Name = \(metricName) , Value = \(self.getValue())>"
     }
 }
