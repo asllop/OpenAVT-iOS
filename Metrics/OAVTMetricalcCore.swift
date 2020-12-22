b0f973d5f0f7be04dd80552f67d3656a1f5a15ea
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-12-22 10:17:22 +0100

    Rewrite type cast.

--- Metrics/OAVTMetricalcCore.swift
+++ Metrics/OAVTMetricalcCore.swift
@@ -19,11 +19,11 @@ open class OAVTMetricalcCore : OAVTMetricalcProtocol {
         var metricArray : [OAVTMetric] = []
         
         if event.getAction() == OAVTAction.START {
-            if let timeSinceMediaRequest = event.getAttribute(key: OAVTAction.MEDIA_REQUEST.getTimeAttribute()) {
-                metricArray.append(OAVTMetric(name: OAVTMetric.START_TIME, type: OAVTMetric.MetricType.Gauge, value: timeSinceMediaRequest as! Int))
+            if let timeSinceMediaRequest = event.getAttribute(key: OAVTAction.MEDIA_REQUEST.getTimeAttribute()) as? Int {
+                metricArray.append(OAVTMetric(name: OAVTMetric.START_TIME, type: OAVTMetric.MetricType.Gauge, value: timeSinceMediaRequest))
             }
-            else if let timeSinceStreamLoad = event.getAttribute(key: OAVTAction.STREAM_LOAD.getTimeAttribute()) {
-                metricArray.append(OAVTMetric(name: OAVTMetric.START_TIME, type: OAVTMetric.MetricType.Gauge, value: timeSinceStreamLoad as! Int))
+            else if let timeSinceStreamLoad = event.getAttribute(key: OAVTAction.STREAM_LOAD.getTimeAttribute()) as? Int {
+                metricArray.append(OAVTMetric(name: OAVTMetric.START_TIME, type: OAVTMetric.MetricType.Gauge, value: timeSinceStreamLoad))
             }
             metricArray.append(OAVTMetric(name: OAVTMetric.NUM_PLAYS, type: OAVTMetric.MetricType.Counter, value: 1))
         }
@@ -32,8 +32,8 @@ open class OAVTMetricalcCore : OAVTMetricalcProtocol {
                 if let inPauseBlock = event.getAttribute(key: OAVTAttribute.IN_PAUSE_BLOCK) as? Bool {
                     if let inSeekBlock = event.getAttribute(key: OAVTAttribute.IN_SEEK_BLOCK) as? Bool {
                         if (inPlaybackBlock && !inPauseBlock && !inSeekBlock) {
-                            if let timeSinceBufferBegin = event.getAttribute(key: OAVTAction.BUFFER_BEGIN.getTimeAttribute()) {
-                                metricArray.append(OAVTMetric(name: OAVTMetric.REBUFFER_TIME, type: OAVTMetric.MetricType.Counter, value: timeSinceBufferBegin as! Int))
+                            if let timeSinceBufferBegin = event.getAttribute(key: OAVTAction.BUFFER_BEGIN.getTimeAttribute()) as? Int {
+                                metricArray.append(OAVTMetric(name: OAVTMetric.REBUFFER_TIME, type: OAVTMetric.MetricType.Counter, value: timeSinceBufferBegin))
                                 metricArray.append(OAVTMetric(name: OAVTMetric.NUM_REBUFFERS, type: OAVTMetric.MetricType.Counter, value: 1))
                             }
                         }
