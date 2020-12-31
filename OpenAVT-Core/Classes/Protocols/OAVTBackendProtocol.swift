e8e21a683d5a80482053bafe0a00ff766f69d3f3
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-12-31 16:50:46 +0100

    Add init method to backend protocol.

--- OpenAVT-Core/Classes/Protocols/OAVTBackendProtocol.swift
+++ OpenAVT-Core/Classes/Protocols/OAVTBackendProtocol.swift
@@ -10,6 +10,18 @@ import Foundation
 
 /// OpenAVT protocol for Backend objects.
 public protocol OAVTBackendProtocol: OAVTComponentProtocol {
+    
+    /**
+     Init with event buffer and metric buffer (optional).
+     
+     - Parameters:
+        - eventBuffer: OAVTBuffer instance for events.
+        - metricBuffer: OAVTBuffer instance for metrics.
+     
+     - Returns: A new backend instance.
+    */
+    init(eventBuffer: OAVTBuffer, metricBuffer: OAVTBuffer?)
+    
     /**
      Send an event.
      
