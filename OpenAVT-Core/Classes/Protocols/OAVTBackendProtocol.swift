30189651e81339e8cc341624d28bebabf1c6df2d
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-12-31 17:13:21 +0100

    Leave initializer definition to specific backend implementations.

--- OpenAVT-Core/Classes/Protocols/OAVTBackendProtocol.swift
+++ OpenAVT-Core/Classes/Protocols/OAVTBackendProtocol.swift
@@ -12,17 +12,6 @@ import Foundation
 public protocol OAVTBackendProtocol: OAVTComponentProtocol {
     
     /**
-     Init with event buffer and metric buffer (optional).
-     
-     - Parameters:
-        - eventBuffer: OAVTBuffer instance for events.
-        - metricBuffer: OAVTBuffer instance for metrics.
-     
-     - Returns: A new backend instance.
-    */
-    init(eventBuffer: OAVTBuffer, metricBuffer: OAVTBuffer?)
-    
-    /**
      Send an event.
      
      - Parameters:
