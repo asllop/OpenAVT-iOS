36008fad4bd462e92daf2a98f35d47f00303d15d
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-01-04 13:17:56 +0100

    Push metrics to graphite.

--- OpenAVT-Graphite.podspec
+++ OpenAVT-Graphite.podspec
@@ -39,4 +39,5 @@ Pod::Spec.new do |s|
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   s.dependency 'OpenAVT-Core'
+  s.dependency 'SwiftSocket'
 end
--- OpenAVT-Graphite/Classes/Backends/OAVTBackendGraphite.swift
+++ OpenAVT-Graphite/Classes/Backends/OAVTBackendGraphite.swift
@@ -6,8 +6,8 @@
 //
 
 import Foundation
-import Network
 import OpenAVT_Core
+import SwiftSocket
 
 /// OAVT backend for Graphite
 open class OAVTBackendGraphite : OAVTBackendProtocol {
@@ -15,24 +15,30 @@ open class OAVTBackendGraphite : OAVTBackendProtocol {
     var host : String
     var port : Int
     var buffer : OAVTBuffer
+    var timeInterval: TimeInterval
     var timer : Timer?
+    
+    private let client : TCPClient
+    private var restartTimer = true
     
     /**
      Init a new OAVTBackendGraphite.
      
      - Parameters:
-        - buffer: An OAVTBuffer instance.
-        - time: time interval between harvest cycles.
-        - host: Graphite server host.
-        - port: Graphite server port.
+     - buffer: An OAVTBuffer instance.
+     - time: time interval between harvest cycles.
+     - host: Graphite server host.
+     - port: Graphite server port.
      
      - Returns: A new OAVTBackendGraphite instance.
-    */
+     */
     public init(buffer: OAVTBuffer = OAVTReservoirBuffer(size: 500), time: TimeInterval = 30.0, host: String, port: Int = 2003) {
         self.buffer = buffer
         self.host = host
         self.port = port
-        self.timer = Timer.scheduledTimer(timeInterval: time, target: self, selector: #selector(pushMetrics), userInfo: nil, repeats: true)
+        self.timeInterval = time
+        self.client = TCPClient(address: host, port: Int32(port))
+        setupTimer(time: time)
     }
     
     deinit {
@@ -52,14 +58,16 @@ open class OAVTBackendGraphite : OAVTBackendProtocol {
     open func endOfService() {
         timer?.invalidate()
         timer = nil
-        pushMetrics()
+        restartTimer = false
+        //TODO: only call if not currently running
+        //pushMetrics()
     }
     
     /**
      Build a list of Graphite metrics using the plaintext format.
      
      - Returns: Array of formated metrics.
-    */
+     */
     open func buildPlaintextMetrics() -> [String] {
         var metrics = [String]()
         for sample in buffer.retrieveInOrder() {
@@ -76,19 +84,53 @@ open class OAVTBackendGraphite : OAVTBackendProtocol {
      Overwrite this method in a subclass to provide a custom metric path.
      
      - Parameters:
-        - metric: An OAVTMetric instance.
+     - metric: An OAVTMetric instance.
      
      - Returns: Metric path.
-    */
+     */
     open func buildMetricName(_ metric: OAVTMetric) -> String {
         return "oavt.\(metric.getName())"
     }
     
-    @objc func pushMetrics() {
-        //TODO: push metrics to graphite
-        OAVTLog.verbose("PUSH TIMER! buffer remaining = \(buffer.remaining())")
-        for m in buildPlaintextMetrics() {
-            OAVTLog.verbose("Metric = \(m)")
+    open func setupTimer(time: TimeInterval) {
+        OAVTLog.verbose("Setup Timer")
+        self.timer?.invalidate()
+        self.timer = nil
+        self.timer = Timer.scheduledTimer(timeInterval: time, target: self, selector: #selector(timerHandler), userInfo: nil, repeats: false)
+    }
+    
+    @objc func timerHandler() {
+        OAVTLog.verbose("Timer Handler")
+        pushMetrics()
+    }
+    
+    open func pushMetrics() {
+        
+        OAVTLog.verbose("Push Metrics! buffer remaining = \(buffer.remaining())")
+        
+        DispatchQueue.global(qos: .background).async {
+            switch self.client.connect(timeout: 10) {
+            case .success:
+                OAVTLog.verbose("Connected to Graphite")
+                for m in self.buildPlaintextMetrics() {
+                    switch self.client.send(string: "\(m)\n" ) {
+                    case .success:
+                        OAVTLog.verbose("Metric Sent")
+                    case .failure(let error):
+                        OAVTLog.error("Failed sending m,etric to Graphite = \(error)")
+                    }
+                }
+            case .failure(let error):
+                OAVTLog.error("Failed connecting to Graphite = \(error)")
+            }
+            
+            self.client.close()
+            
+            if self.restartTimer {
+                DispatchQueue.main.async {
+                    self.setupTimer(time: self.timeInterval)
+                }
+            }
         }
     }
 }
